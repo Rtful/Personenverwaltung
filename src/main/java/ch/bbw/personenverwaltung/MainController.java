ee51fc416e607a74d9ebac8041dde98262c7909e
Aaron Holenstein <4aron.dodger@gmail.com>
2022-06-30 11:07:08 +0200

    db stuff

--- src/main/java/ch/bbw/personenverwaltung/MainController.java
+++ src/main/java/ch/bbw/personenverwaltung/MainController.java
@@ -1,5 +1,6 @@
 package ch.bbw.personenverwaltung;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
+    private PersonRepository personRepository;
+
+    @Autowired
+    public MainController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
     @GetMapping("/index")
     public String viewIndex() {
